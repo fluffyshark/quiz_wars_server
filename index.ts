19743d074da52cd26500e611c014d6b2a3faae34
fluffyshark <robin.yjord1@gmail.com>
2023-01-28 20:30:56 +0100

    Socket - Relevant RegionData have been added, which are updated and sent to user every time a point is received

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ const io = require('socket.io')(http, {cors: {origin: true, credentials:true, op
 
 import {gameDataObject} from "./utilities/gameData"
 import {updateRegionPoints} from "./utilities/updateGameData"
+import {getIndexByGamecode} from "./utilities/getIndexByGameCode"
 
 interface SocketEvents {
     on: (event:string, callback: (data: any) => void) => void;
@@ -26,11 +27,13 @@ io.on("connection", (socket:SocketEvents) => {
     socket.emit("testing_from_server", "OK from server");
   });
 
+
   // Receiving points and region destination from user
   socket.on("user_got_point", (pointData:{userName:string, points:number, regionId:number, gameCode:number, team:string}) => {
-
     // Adds user point to GameData | ./utilities/updateGameData.ts
     updateRegionPoints(pointData)
+    // Send updated regions part of GameData to users | Will later be all users in a room based on gameCode
+    socket.emit("send_gamedata_to_users", gameDataObject[getIndexByGamecode(pointData.gameCode)].regions);
 
     console.log("user_got_point: ", pointData)
   });
