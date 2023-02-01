b099048363ab897919e033086dc9c88c63b8da97
fluffyshark <robin.yjord1@gmail.com>
2023-02-01 05:45:44 +0100

    Socket - Host can create socket room using game code, users can request game codes to prevent writing non-existant thus joining an empty room

--- index.ts
+++ index.ts
@@ -27,16 +27,45 @@ io.on("connection", (socket:SocketEvents) => {
   //  console.log("user connected")
 
   
-// Host create socket room (game code) and a new game data object
+  // Host create socket room (game code) and a new game data object
   socket.on("host_create_room", (gameCode) => {
     // Host join (and create) room
     socket.join(gameCode); 
     // Creating new game data object for the game
     newGameData(socket.id, gameCode)
+    console.log(gameDataObject)
+    console.log(gameDataObject.length)
+
+  /*
+  // If game is ongoing then players who join will immedietly enter game
+    if (gameDataObject[getIndexByGamecode(gameCode)].gameStatus === "game_ongoing") {
+      // Declaring list of players
+      let newPlayerData = gameDataObject[getIndexByGamecode(gameCode)].users
+      // Tell player to start game, sending gameData
+      io.to(socket.id).emit("start_game", newPlayerData); 
+    }
+    */
+  
   });
 
 
+
+  // Send users a all gameCodes, to validate their game code input, to make sure that they enter a socket room created by host 
+  socket.on("user_requesting_all_gamecodes", () => { 
+    let allGameCodes = []
+    for (let i = 0; i < gameDataObject.length; i++) {allGameCodes.push(gameDataObject[i].gameCode)}
+    socket.emit("sending_user_gamecodes_request", allGameCodes);
+  })
+
+
+// When player join, then player data is sent to host, player nickname are displayed on host screen
   socket.on("player_joining", (userInfo: {username:string, gameCode:string, team:string}) => { 
+    // User join room
+    socket.join(userInfo.gameCode); 
+    // Sending joined player data to host, to be used to display player name on screen
+    socket.to(userInfo.gameCode).emit("player_accepted", userInfo);
+    // Sending player their socket id
+  //  io.to(socket.id).emit("sending_playerID", socket.id);
     // Adding joined player to game data
     gameDataObject[getIndexByGamecode(userInfo.gameCode)].users.push({userId: socket.id, username: userInfo.username, points: 0, gameCode: userInfo.gameCode, team: userInfo.team})
     console.log("player_joining", gameDataObject[getIndexByGamecode(userInfo.gameCode)].users)
